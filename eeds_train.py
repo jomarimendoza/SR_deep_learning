e711a0ad7e1ee41178da5afd02923bd77fe06dd5
deeplearning2 <deeplearning2@deeplearning2>
2017-12-29 19:45:14 +0800

    weights shallow network

--- eeds_train.py
+++ eeds_train.py
@@ -42,7 +42,7 @@ def train():
     print('testing shape: ',x_valid.shape, y_valid.shape)
 
     # Load the model
-    inputs = Input(shape=(24,24,1))
+    inputs = Input(shape=(None,None,1))
 
     # create proposed model, with ResNet weights frozen
     model = EEDS_model(inputs=inputs)
